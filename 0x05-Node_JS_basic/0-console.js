eafc5dc15fa750c3abc3337ba3cd221c56764b28
AltechInferno <altechinferno@gmail.com>
2024-02-26 23:45:50 +0000

    task0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on STDOUT
+ * @param {String} msg The message to display.
+ */
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
